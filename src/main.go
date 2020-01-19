31a9e075813b690a49b7f2a6e7b64ad7237dfbf8
patrickz98 <patrickz98@users.noreply.github.com>
2020-01-19 16:34:51 +0100

    Check in.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/patrickz98/Hashcode2020Quali/src/order"
+)
 
-func main()  {
-	fmt.Println("hallo")
+func main() {
+
+	inputFile := "b_small"
+	constrains := order.New(inputFile)
+
+	fmt.Println("SlicesMaximum", constrains.SlicesMaximum)
+	fmt.Println("Slices", constrains.Slices)
+
+	score := constrains.Submission([]int{0, 1})
+
+	fmt.Println("Score", score)
 }
---
+++ src/order/parse.go
@@ -0,0 +1,81 @@
+package order
+
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Constrains struct {
+	inputFile     string
+	SlicesMaximum int
+	Size          int
+	Slices        []int
+}
+
+const submissonPath = "submisson"
+
+func (cons Constrains) Submission(indices []int) int {
+
+	sum := 0
+
+	submissonTxt := strconv.Itoa(len(indices)) + "\n"
+	indicesStr := make([]string, len(indices))
+
+	for inx, index := range indices {
+		sum += cons.Slices[index]
+		indicesStr[inx] = strconv.Itoa(index)
+	}
+
+	submissonTxt += strings.Join(indicesStr, " ") + "\n"
+
+	err := os.MkdirAll(submissonPath, 0755)
+	check(err)
+
+	outFile := submissonPath + "/" + cons.inputFile + ".out"
+	err = ioutil.WriteFile(outFile, []byte(submissonTxt), 0755)
+	check(err)
+
+	return sum
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func New(inputFile string) Constrains {
+
+	bytes, err := ioutil.ReadFile("input/" + inputFile + ".in")
+	check(err)
+
+	input := strings.Split(string(bytes), "\n")
+	constrains := strings.Split(input[0], " ")
+
+	slicesMaximum, err := strconv.Atoi(constrains[0])
+	check(err)
+
+	typesOfPizza, err := strconv.Atoi(constrains[1])
+	check(err)
+
+	pizzaParts := strings.Split(input[1], " ")
+
+	pizzaSlices := make([]int, typesOfPizza)
+
+	for inx, pizzaStr := range pizzaParts {
+
+		pizzaSlice, err := strconv.Atoi(pizzaStr)
+		check(err)
+
+		pizzaSlices[inx] = pizzaSlice
+	}
+
+	return Constrains{
+		inputFile:     inputFile,
+		SlicesMaximum: slicesMaximum,
+		Size:          typesOfPizza,
+		Slices:        pizzaSlices,
+	}
+}
